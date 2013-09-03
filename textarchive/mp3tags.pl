35577cc2344ab4d0a4536f078a05db9e339dcbf3
Gerhard Gonter <ggonter@gmail.com>
2013-09-03 16:59:12 +0200

    added some remarks about mp3 information

--- textarchive/mp3tags.pl
+++ textarchive/mp3tags.pl
@@ -28,3 +28,44 @@ foreach my $file (@PAR)
 }
 
 
+__END__
+
+=head1 Dependencies
+
+Ubuntu:
+  apt-get install libmp3-info-perl
+
+=begin comment
+
+there is more interesting stuff in mp3 files, for example:
+
+nbgg:gonter> mp3tags.pl '_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3'
+file: [_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3]
+tag: $VAR1 = {
+  'YEAR' => '1987',
+  'ARTIST' => 'Roger Waters',
+  'COMMENT' => '',
+  'ALBUM' => 'Radio K.A.O.S.',
+  'TITLE' => 'The Tide Is Turning',
+  'GENRE' => 'Progressive Rock',
+  'TRACKNUM' => '8',
+  'TAGVERSION' => 'ID3v2.3.0'
+};
+nbgg:gonter> mp3info '_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3'
+_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3 does not have an ID3 1.x tag.
+
+nbgg:gonter> mp3info -x '_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3'
+_in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3 does not have an ID3 1.x tag.
+File: _in_2013-05-25_Rock/Roger Waters/Radio K.A.O.S/08-The Tide Is Turning.mp3
+Media Type:  MPEG 1.0 Layer III
+Audio:       320 kbps, 44 kHz (joint stereo)
+Emphasis:    none
+CRC:         No
+Copyright:   No
+Original:    No
+Padding:     Yes
+Length:      5:44
+
+=end comment
+=cut
+
