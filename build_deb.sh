c0f2e0faeaf262f12bc73484a0637ab796c1198c
Gerhard Gonter <ggonter@gmail.com>
2023-02-17 14:24:51 +0100

    added build files for debian packaging

---
+++ build_deb.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+epoch=0
+pkgname=`awk '/Package:/ {print $2}' debian/control`
+version=`awk '/Version:/ {print $2}' debian/control`
+pkgdir=tmp/$pkgname/$epoch/$version
+bindir=$pkgdir/data/usr/bin
+
+pmlnk.pm --copy --target $pkgdir/data/usr/share/perl5 .
+mkdir $pkgdir/control
+cp debian/control $pkgdir/control
+echo '2.0' > $pkgdir/debian-binary
+
+mkdir -p $bindir
+cp textarchive/vlib001.pl textarchive/mp3tags.pl textarchive/oggtags.pl $bindir
+
+mkdeb.pl $pkgdir
+
